194a56a9ab5d0a5b2b2cd19360d0ad63d210e91f
DARSEN12 <faragonc@unsa.edu.pe>
2025-05-24 14:24:28 -0500

    Inicializando y analizado el ejercicio desarrollado 1

---
+++ Sesión 3/ED2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ED2 {
+    public static void main(String[] args) {
+        ArrayList<String> alumnos = new ArrayList<String>();
+        ArrayList<Integer> notas = new ArrayList<Integer>();
+        alumnos.add("MARIA");
+        alumnos.add("DIEGO");
+        alumnos.add("RENE");
+        alumnos.add("ALONSO");
+        System.out.println(alumnos.hashCode());
+        System.out.println(alumnos.isEmpty());
+        System.out.println(alumnos.size());
+        
+        Iterator<String> itA = alumnos.iterator();
+        while (itA.hasNext()) {
+        System.out.println(itA.next());
+        }
+    }
+}
